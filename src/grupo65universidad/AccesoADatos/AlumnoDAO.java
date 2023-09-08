333862d25c30d23575f19ab841f5f7ef12d6770e
Azoka23 <marcela68.ma@gmail.com>
2023-09-08 11:17:45 -0300

    extends

--- src/grupo65universidad/AccesoADatos/AlumnoDAO.java
+++ src/grupo65universidad/AccesoADatos/AlumnoDAO.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package grupo65universidad.AccesoADatos;
 
-/**
- *
- * @author marcelaaliciaarroyo
- */
-public class AlumnoDAO {
+
+public class AlumnoDAO extends DAO{
     
 }
