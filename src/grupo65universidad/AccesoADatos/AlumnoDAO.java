3622f7f5e46b2fa8f78af1e8d051f8d29c128a22
Azoka23 <marcela68.ma@gmail.com>
2023-09-08 11:50:06 -0300

    metodo guardar

--- src/grupo65universidad/AccesoADatos/AlumnoDAO.java
+++ src/grupo65universidad/AccesoADatos/AlumnoDAO.java
@@ -1,7 +1,27 @@
-
 package grupo65universidad.AccesoADatos;
 
+import grupo65universidad.Entidades.Alumno;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 
-public class AlumnoDAO extends DAO{
-    
+public class AlumnoDAO extends DAO {
+
+    public void guardarAlumno(Alumno alumno) throws Exception {
+        try {
+            if (alumno == null) {
+                throw new Exception("Debes indicar un alumno");
+            }
+            String sql = "INSERT INTO alumnos (dni,apellido,nombre,fechaNacimiento,estado)"
+                    + "VALUES (?,?,?,?,?)";
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
+            ps.setBoolean(5, alumno.isEstado());
+            insertarModificarEliminar(sql);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
