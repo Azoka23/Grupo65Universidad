2f71fa04cfac82f10c1bd8b7c2b524c73eac5c37
Azoka23 <marcela68.ma@gmail.com>
2023-09-11 11:55:37 -0300

    cambios vista

--- src/grupo65universidad/Grupo65Universidad.java
+++ src/grupo65universidad/Grupo65Universidad.java
@@ -1,6 +1,8 @@
 
 package grupo65universidad;
 
+import grupo65universidad.Vistas.IngresoAlSistema;
+
 
 public class Grupo65Universidad {
 
@@ -8,7 +10,9 @@ public class Grupo65Universidad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        IngresoAlSistema ingreso=new IngresoAlSistema();
+        ingreso.setVisible(true);
+        ingreso.setLocationRelativeTo(null);
     }
     
 }
