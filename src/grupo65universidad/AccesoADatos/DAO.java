da2138d742ce56d43bd129748818daa30bfc6f51
AtrumBlack <walterbasanta66@gmail.com>
2023-09-13 10:35:55 -0300

    DAO-Modificados
    Modificado-DAO

--- src/grupo65universidad/AccesoADatos/DAO.java
+++ src/grupo65universidad/AccesoADatos/DAO.java
@@ -5,16 +5,16 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Set;
 import javax.swing.JOptionPane;
-import java.sql.PreparedStatement;
 
 public class DAO {
 
     protected Connection conexion = null;
     protected ResultSet resultado = null;
     protected Statement sentencia = null;
-   
+
     private final String USUARIO = "root";
     private final String PASSWORD = "";
     private final String DB = "universidad";
@@ -24,16 +24,16 @@ public class DAO {
     protected void conectarBase() throws ClassNotFoundException, SQLException {
         try {
             Class.forName(DRIVER);
-            conexion = DriverManager.getConnection(URL+DB+"?useSSL=false&serverTimezone=UTC", USUARIO, PASSWORD);
-            JOptionPane.showMessageDialog(null, "Se conecto a la base de datos : "+DB);
+            conexion = DriverManager.getConnection(URL + DB + "?useSSL=false&serverTimezone=UTC", USUARIO, PASSWORD);
+           
         } catch (ClassNotFoundException | SQLException ex) {
-            //JOptionPane.showMessageDialog(null, "Error al conectarse");
-            throw ex ;
+ 
+            throw ex;
         }
     }
 
-    protected void desconectarBase()  {
-        try{
+    protected void desconectarBase() {
+        try {
             if (resultado != null) {
                 resultado.close();
             }
@@ -51,30 +51,52 @@ public class DAO {
     protected void insertarModificarEliminar(String sql) throws ClassNotFoundException {
         try {
             conectarBase();
-            sentencia=conexion.createStatement();
+            sentencia = conexion.createStatement();
             sentencia.executeUpdate(sql);
         } catch (SQLException ex) {
-            //conexion.rollback();//vuelve atras y omite todo
-            //set.autocommit = 1;
-            //Commit;
+
             JOptionPane.showMessageDialog(null, "Ejecucion satisfactoria");
-            
+
         } finally {
             desconectarBase();
         }
     }
-    
-    protected void consultarBase(String sql) throws Exception{
-    try{
-    conectarBase();
-    //JOptionPane.showMessageDialog(null, "Estamos despues de conectar la base"+sql);
-    sentencia=conexion.createStatement();
-    //JOptionPane.showMessageDialog(null, "Estamos despues de la sentencia"+sql);
-    resultado=sentencia.executeQuery(sql);
-    }catch(Exception e){
-       // JOptionPane.showMessageDialog(null, "consulta fallida");
-    throw e;
+
+    protected void consultarBase(String sql) {
+        try {
+            conectarBase();
+            sentencia = conexion.createStatement();
+            resultado = sentencia.executeQuery(sql);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "consulta exitosa");
+        }
     }
+
+    protected ResultSet consultarBase(PreparedStatement preparedStatement) {
+        try {
+            // Ejecutar la consulta
+            ResultSet resultado = preparedStatement.executeQuery();
+            return resultado;
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "No encontro el DNI");
+            return null; // Devuelve null en caso de error
+        }
+    }
+
+    protected void insertarModificarEliminar(PreparedStatement preparedStatement) throws ClassNotFoundException {
+        try {
+
+            // En su lugar, ejecuta la PreparedStatement que recibiste como argumento
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "No se ejecuto");
+
+        } finally {
+            desconectarBase();
+        }
     }
 
 }
